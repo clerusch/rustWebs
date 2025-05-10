59efdaa227113d58d9688fd2a04077b370f64e7c
Clemens <clemensrubenschumann@googlemail.com>
2025-05-10 20:34:49 +0800

    Added cool autorun functionality to vscode

--- rustWeb/src/main.rs
+++ rustWeb/src/main.rs
@@ -1,9 +1,18 @@
 mod test_fusion;
 mod tikz_export;
 use std::env;
+use quizx::{graph::GraphLike, hash_graph::Graph};
+use std::fs::write;
+use quizx::hash_graph::VType::Z;
 fn main(){
     println!("Running from: {:?}", env::current_dir().unwrap());
-    // test_fusion::test_fusion();
-    let g = test_fusion::create_chain(10);
-    tikz_export::export_to_tikz(&g, "../../masterarbeit/figures/graph.tex");
+    let mut g = Graph::new();
+    let a = g.add_vertex(Z);
+    let b = g.add_vertex(Z);
+    g.add_edge(a, b);
+    // let g = test_fusion::create_chain(10);
+    // let dot = g.to_dot();
+    let _ = write("target/debug/examples/graphtest.dot", g.to_dot());
+    test_fusion::test_fusion();
+    
 }
